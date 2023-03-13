c59cf5786a0107bf3c578f4174adff3560976996
SadovskiiAleks <alekssadovskij@yandex.ru>
2023-03-13 21:07:28 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -4,8 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 
 public class PhoneBook {
-
     private int quantityOfContacts = 0;
+
     private HashMap<String,String> phoneBook = new HashMap<>();
     public int add(String name, String number){
         if (!phoneBook.containsKey(name)) {
