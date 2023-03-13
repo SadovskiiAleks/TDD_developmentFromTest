84acd23d76fcf77cdf6c384711d385e139d7ae3a
SadovskiiAleks <alekssadovskij@yandex.ru>
2023-03-13 21:06:29 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class PhoneBook {
 
+    private int quantityOfContacts = 0;
+    private HashMap<String,String> phoneBook = new HashMap<>();
     public int add(String name, String number){
-        return 0;
+        if (!phoneBook.containsKey(name)) {
+            phoneBook.put(name,number);
+            return ++quantityOfContacts;
+        } else {
+            return quantityOfContacts;
+        }
     }
 }
